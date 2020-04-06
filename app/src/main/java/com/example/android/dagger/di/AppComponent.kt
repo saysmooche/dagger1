e42c67b0cf7422c5ac5bba8f7f2c8df157615399
Field Employee <fieldemployee@Fields-MacBook-Pro-4.local>
2020-04-06 10:28:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.user.UserManager
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
+    fun userManager(): UserManager
+}
